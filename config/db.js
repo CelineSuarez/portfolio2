6c00f9c8a6036dba755c68862559284e32640b03
CelineSuarez <celinesuarez7@gmail.com>
2016-12-07 18:07:45 -0600

    Fixed the incorrect code being in routes.js when it should have been in server.js.  Also added a procfile and setup mLab connection.

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://localhost/stencil-dev'
+        url : 'mongodb://heroku_bkkplgcx:j7hn6ttt2q91lhi9r4l8imue4u@ds127978.mlab.com:27978/heroku_bkkplgcx'
     }
